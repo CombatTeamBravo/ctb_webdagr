8ed8c48850536615520a8efc7c92f2b4fd20c68d
OpenShift guest <craig.foley@gmail.com>
2014-06-17 21:53:25 -0400

    Back to a simple example, c9 compatible

--- app.js
+++ app.js
@@ -1,55 +1,64 @@
 
+// our web server's IP and port
+var httpPort= 8080;
+var httpIP= 'localhost';
+// Are we running inside cloud 9?
+var isCloud9= false;
+if (process.argv[2]=== 'c9') {
+    isCloud9= true;
+    // Use Cloud9's Port and IP for hosting the express web server...
+    httpPort= process.env.PORT;
+    httpIP= process.env.IP;
+};
+    
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes')
+  , http= require('http')
+  , path= require('path')
   ,	socketio = require('socket.io')
   , dgram = require('dgram')
   ,	udpServer = dgram.createSocket('udp4')
-  , udpPort = 32114;
+  , udpPort = 32114;        // Port we listen on, for incoming messages from A3.
 
-var app = module.exports = express.createServer();
+var app = express();
+var server= http.createServer(app);
+var io= socketio.listen(server);
 
-// Configuration
+// Base web files directory is ./web
+app.use(express.static(path.resolve(__dirname, 'web')));
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(8080, function(){
-  console.log("HTTP server listening on port %d in %s mode", app.address().port, app.settings.env);
+server.listen(httpPort, httpIP, function(){
+  console.log("HTTP server listening on port %d", server.address().port);
 });
 
 /**********************
 Axyl's code starts here
 **********************/
 
-// Storing the created IO socket... (as ioInstance) so we can send it messages in the UDP's message received event
-var ioInstance = socketio.listen(app).on('connection', function (socket) {
-    socket.on('message', function (msg) {
-        console.log('Message Received: ', msg);
-        socket.broadcast.emit('message', msg);
+var wsockets= [];        // Array of connected web sockets.
+
+io.on('connection', function (wsocket) {
+    console.log('Web Socket connected.');
+    // New Web Socket?  Add it to the array list of web sockets.
+    wsockets.push(wsocket);
+    
+    // When they've disconnecteD?  remove them...
+    wsocket.on('disconnect', function() {
+        wsockets.splice(wsockets.indexOf(wsocket), 1);
+        console.log('Web Socket disconnected.');
     });
 });
+
+// Call this to broadcast a web socket message to all connected clients.
+function broadcastWS(event, data) {
+    wsockets.forEach(function (wsocket) {
+        wsocket.emit(event, data);
+    });
+}
 
 // code triggered when our UDP Server starts listening for updates...
 udpServer.on('listening', function() {
@@ -58,12 +67,36 @@ udpServer.on('listening', function() {
 });
 
 // Turn on the UDP server
-udpServer.bind(udpPort);
+if (!isCloud9) {
+    udpServer.bind(udpPort);
+};
 
 //code triggered whenever we receive a message from our UDP server.
 udpServer.on('message', function(message, remoteSender) {
 	var posData = JSON.parse(message);
 	console.log('Client at XPos: '+ posData.x+ ', YPos: '+ posData.y);
 	// So we've received an update, now broadcast that to our web page via a web socket...
-	ioInstance.emit('message', posData);
+	broadcastWS('message', posData);
 });  // End of onMessage event.
+
+// If we're running on Cloud9, then we can't receive UDP messages, so just fake it.
+function sendFakeUpdate() {
+    var fakePosData= {
+        x:"0343", 
+        y: "3220",
+        alt:Math.floor((Math.random() * 20) + 1),
+        dir:Math.floor((Math.random() * 360) + 1),
+        spd:Math.floor((Math.random()* 30)+ 1),
+        tme:'12:00'
+    };
+	console.log('Sending fake POS Data at XPos: '+ fakePosData.x+ ', YPos: '+ fakePosData.y);
+	// So we've received an update, now broadcast that to our web page via a web socket...
+	broadcastWS('message', fakePosData);
+}
+
+if (isCloud9) {
+    // Trigger fake updates every 10 seconds.
+    var runIntervalCode= setInterval(sendFakeUpdate, 10000);
+    //and an initial broadcast.
+    sendFakeUpdate();
+}
